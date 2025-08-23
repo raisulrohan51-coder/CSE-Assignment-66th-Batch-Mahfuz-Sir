7d1b33dd228a27211e11468d581d3bd987bd2527
raisulrohan51-coder <raisulrohan51@gmail.com>
2025-08-23 21:53:55 +0600

    Add files via upload

---
+++ 21.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+   char i;
+    cout << "Alphabets from A to Z: ";
+    for (i = 'A'; i <= 'Z'; i++)
+    {
+        cout << i << " " ;
+
+    }
+    return 0 ;
+}
